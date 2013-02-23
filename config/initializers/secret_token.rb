d422ca80955f01dff367944b5dbe0da680f6c164
KolobocK <kolobock@kolobock.lan>
2013-02-23 20:47:59 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushPuzzle::Application.config.secret_token = 'f9ae73caf9a7fadfe2bb5a343e0e64e64f118b66fc848162445da51e76fcd3f63a7c13e3072b73721662f2fbcbeec6643bbeadc02091abf55dd767c4943d67c7'
