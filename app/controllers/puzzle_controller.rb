bfbb23d571460459ba0334cbdf98576f99c34eaa
andrii rudenko <kolobock@gmail.com>
2013-02-25 00:51:54 +0200

    add puzzle#new. get puzzles from Puzzle model

--- app/controllers/puzzle_controller.rb
+++ app/controllers/puzzle_controller.rb
@@ -1,5 +1,11 @@
 class PuzzleController < ApplicationController
   def index
-    @puzzles = (0..15).to_a.shuffle
+    # @puzzles = (0..15).to_a.shuffle
+    @puzzles = Puzzle.new.generate_puzzles
+  end
+
+  def new
+    @puzzles = Puzzle.new.generate_puzzles
+    render json: @puzzles
   end
 end
